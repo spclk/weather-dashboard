0bde69daf0f7e7b3009f1d5d18cfec7410f88d09
Konstantin <konstantin1020@gmail.com>
2021-02-18 12:20:49 -0800

    html changes

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ var forecast3 = document.querySelector("#day3");
 var forecast4 = document.querySelector("#day4");
 var forecast5 = document.querySelector("#day5");
 
-var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" ;
-var weatherKey = "910119384e38823f5fe0923ddc62a2c5";
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var weatherKey = "&units=imperial&appid=910119384e38823f5fe0923ddc62a2c5";
 
-var cityName = "";
+var cityName = ""; 
 
-
-// &appid
+function todaysWeather (city){
+  cityName = city;
+  var query = weatherURL + cityName + weatherKey;
+  fetch(query).
+    then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
 searchEntry.addEventListener("click", function(event){
-  var clicked = $(event.target).attr("id");
-  var siblingTextArea = $(event.target).prev().val();
-  console.log(clicked,siblingTextArea);
+  var searchCity = $("#search").val()
+  todaysWeather(searchCity)
+  var recentButton = document.createElement("button");
+  recentButton.textContent = searchCity
+  searchHistory.appendChild(recentButton)
 });
 
-// localStorage.seItem();
+// localStorage.setItem();
 // localStorage.getItem();
